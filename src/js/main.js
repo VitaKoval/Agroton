296941a99bffeda30e8be0d12a2c1aafad42a167
Vita Koval <mayevit@hotmail.com>
2025-06-16 13:17:08 -0600

    burger mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
 console.log('main.js');
+// Burger menu
+const burger = document.querySelector('.js-burger');
+const mobileMenu = document.querySelector('.js-mobile-menu');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
+})
